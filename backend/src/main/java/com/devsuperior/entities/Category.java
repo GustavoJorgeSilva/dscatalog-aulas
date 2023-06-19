5afb1f142e2fbef208fb0d2a163043138e177d7f
GustaVoJorgeSilva <gustavo.jorge@hotmail.com.br>
2023-06-19 14:40:01 -0300

    CategoryResource

---
+++ backend/src/main/java/com/devsuperior/entities/Category.java
@@ -0,0 +1,49 @@
+package com.devsuperior.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
+
+public class Category implements Serializable {
+
+
+    private UUID id;
+    private String name;
+
+
+    public Category(){
+
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Category category)) return false;
+        return Objects.equals(getId(), category.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
