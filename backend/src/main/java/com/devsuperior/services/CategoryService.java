05d50292827cf2a34ed5c4f3933667a9cdba1b9a
GustaVoJorgeSilva <gustavo.jorge@hotmail.com.br>
2023-06-20 11:54:18 -0300

    DataBase seeding

--- backend/src/main/java/com/devsuperior/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/services/CategoryService.java
@@ -3,6 +3,7 @@ package com.devsuperior.services;
 import com.devsuperior.entities.Category;
 import com.devsuperior.repositories.CategoryRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,6 +16,7 @@ public class CategoryService {
         this.categoryRepository = categoryRepository;
     }
 
+    @Transactional(readOnly = true)
     public List<Category> findAll(){
         return categoryRepository.findAll();
     }
---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_category(name) VALUES ('Books')
+INSERT INTO tb_category(name) VALUES ('Eletrocnics')
+INSERT INTO tb_category(name) VALUES ('Computers')
